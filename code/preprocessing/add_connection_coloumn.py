785592b9b6c8d1cf6aeda1bd8391b8c1f3769a04
Alessandro Macaro <alessandro.macaro@gmail.com>
2024-10-02 10:43:12 +0200

    update

--- code/preprocessing/add_connection_coloumn.py
+++ code/preprocessing/add_connection_coloumn.py
@@ -1,51 +1,50 @@
 import csv
 import os
-import glob
 
 
 def load_unique_connections(txt_file):
     unique_connections = set()
-
-    # Leggi il file input contenente le coppie di connessioni uniche
     with open(txt_file, mode='r') as file:
         for line in file:
-            # Estrai Source IP e Destination IP
             source_ip, destination_ip = line.strip().split(', ')
             connection = tuple(sorted([source_ip, destination_ip]))
             unique_connections.add(connection)
-
     return unique_connections
 
 
-def extract_attack_label(csv_filename):
-    # Estrai il nome del file senza il percorso
-    base_name = os.path.basename(csv_filename)
-    # Ottengo l'etichetta dell'attacco
-    attack_label = base_name.split('_')[0].lower()
-    return attack_label
+def determine_attack_category(directory_path):
+    """
+    Determine the attack category based on the directory path as it is.
+    """
+    # Check against the exact strings
+    if 'Flooding attack' in directory_path:
+        return "Denial of Service"
+    elif 'Port Scan' in directory_path or 'OS Scan' in directory_path or 'Host Discovery' in directory_path:
+        return "Network Scanning"
+    elif 'OpenVAS_scan' in directory_path:
+        return "Vulnerability Scanning"
+    else:
+        return "Benign"  # Default category
 
 
-def add_connection_column(csv_input_file, csv_output_file, unique_connections, attack_label, process_type):
-    # Leggi il file CSV in input e creane uno nuovo con una colonna aggiuntiva "Type of connection"
+def add_connection_column(csv_input_file, csv_output_file, unique_connections, attack_category, process_type):
     with open(csv_input_file, mode='r', newline='') as csvfile_input:
         reader = csv.DictReader(csvfile_input)
-        # Aggiungi la nuova colonna
         fieldnames = reader.fieldnames + ['Type of connection']
 
         with open(csv_output_file, mode='w', newline='') as csvfile_output:
             writer = csv.DictWriter(csvfile_output, fieldnames=fieldnames)
             writer.writeheader()
 
-            # Per ciascuna riga del file di input CSV, controlla se la connessione è legittima
             for row in reader:
                 source_ip = row['Source IP'].strip()
                 destination_ip = row['Destination IP'].strip()
 
                 connection = tuple(sorted([source_ip, destination_ip]))
 
-                # Assegna il valore per "Type of connection"
+                # Assign the "Type of connection" value
                 if process_type == 'Multiclass':
-                    row['Type of connection'] = "Benign" if connection in unique_connections else attack_label
+                    row['Type of connection'] = "Benign" if connection in unique_connections else attack_category
                 elif process_type == 'Binary':
                     row['Type of connection'] = "0" if connection in unique_connections else "1"
 
@@ -54,30 +53,29 @@ def add_connection_column(csv_input_file, csv_output_file, unique_connections, a
 
 def process_directory(directory_path, txt_unique_connections, process_type):
     """
-    Cerca ed elabora tutti i file CSV all'interno della cartella specificata e delle sottocartelle.
+    Process all CSV files within the specified directory and subdirectories to add the "Type of connection" column.
 
-    :param directory_path: Percorso della cartella principale da elaborare.
-    :param txt_unique_connections: Percorso del file contenente le connessioni uniche.
-    :param process_type: Tipo di processo ('Multiclass' o 'Binary').
+    :param directory_path: Path to the main directory containing the CSV files.
+    :param txt_unique_connections: Path to the file containing the unique connections.
+    :param process_type: Process type ('Multiclass' or 'Binary').
     """
-    # Carica le coppie di connessioni uniche una volta sola
     unique_connections = load_unique_connections(txt_unique_connections)
 
-    # Cammina attraverso la cartella e tutte le sottocartelle
+    # Iterate through all files in the directory and subdirectories
     for root, dirs, files in os.walk(directory_path):
         for file in files:
-            # Considera solo i file con estensione .csv
             if file.endswith(".csv"):
                 csv_input_file = os.path.join(root, file)
-                csv_output_file = os.path.join(root, file.replace('.csv', '_' + process_type.lower() + '.csv'))
+                csv_output_file = os.path.join(root, file.replace('.csv', f'_{process_type.lower()}.csv'))
 
-                attack_label = extract_attack_label(csv_input_file)
-                add_connection_column(csv_input_file, csv_output_file, unique_connections, attack_label, process_type)
-                os.remove(csv_input_file)
-                print(f"File originale eliminato: {csv_input_file}")
+                # Determine attack category based on directory structure
+                attack_category = determine_attack_category(root)
+
+                add_connection_column(csv_input_file, csv_output_file, unique_connections, attack_category, process_type)
+                # os.remove(csv_input_file)
+                print(f"Processed file: {csv_input_file} into {csv_output_file}")
 
 
-# Esegui lo script specificando la directory contenente i CSV e il file delle connessioni uniche
+# Execute the script by specifying the directory containing the CSV files and the file with unique connections
 process_directory('/home/alessandro/Scrivania/UNISA - Magistrale/Tesi/Attacks csv',
-                  '/home/alessandro/Scrivania/UNISA - Magistrale/Tesi/Attacks csv/connessioni_uniche.txt', process_type='Binary')
-# DA FARE MULTICLASS
+                  '/home/alessandro/Scrivania/UNISA - Magistrale/Tesi/Attacks csv/connessioni_uniche.txt', process_type='Multiclass')
--- code/preprocessing/clean_csv_data.py
+++ code/preprocessing/clean_csv_data.py
@@ -1,20 +1,20 @@
 import os
 import pandas as pd
 
-
 def remove_empty_rows_in_csv(directory):
-    # Cerca tutti i file CSV all'interno della directory e delle sottocartelle
     for root, _, files in os.walk(directory):
         for filename in files:
             if filename.endswith('.csv'):
                 file_path = os.path.join(root, filename)
 
-                # Leggi il CSV
                 df = pd.read_csv(file_path)
 
-                # Controlla se ci sono righe con valori vuoti
                 initial_row_count = len(df)
-                df = df.dropna()  # Rimuove le righe con valori NaN
+                df = df.dropna(subset=['Source IP', 'Destination IP'])
+
+                # Riempie le altre colonne con "N/A" se "Source IP" o "Destination IP" sono presenti
+                mask = df['Source IP'].notna() | df['Destination IP'].notna()
+                df.loc[mask] = df.loc[mask].fillna('N/A')
 
                 # Verifica se ci sono state modifiche
                 if len(df) < initial_row_count:
