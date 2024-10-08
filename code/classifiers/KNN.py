687840e34f70a8f8681acbb0bff1995b88300783
Alessandro Macaro <alessandro.macaro@gmail.com>
2024-10-08 11:03:33 +0200

    update classifiers

--- code/classifiers/KNN.py
+++ code/classifiers/KNN.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
@@ -23,13 +23,32 @@ def load_and_preprocess_data(csv_file, test_size=0.2):
     preprocessor = ColumnTransformer(transformers=[('num', MinMaxScaler(), numeric_features)])
 
     # Suddivisione in training set e test set con stratificazione
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify=y, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify=y, shuffle=True)
 
     # Applica il preprocessing
     X_train = preprocessor.fit_transform(X_train)
     X_test = preprocessor.transform(X_test)
 
     return X_train, y_train, X_test, y_test
+
+def evaluate_model(y_true, y_pred, dataset_type='Test'):
+    # Calcola le metriche di valutazione
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, average='weighted', zero_division=0)
+    recall = recall_score(y_true, y_pred, average='weighted', zero_division=0)
+    f1 = f1_score(y_true, y_pred, average='weighted', zero_division=0)
+
+    # Stampa i risultati
+    print(f"\nRisultati sul {dataset_type}:")
+    print(f"Accuracy del {dataset_type}: {accuracy:.4f}")
+    print(f"Precisione del {dataset_type}: {precision:.4f}")
+    print(f"Recall del {dataset_type}: {recall:.4f}")
+    print(f"F1 Score del {dataset_type}: {f1:.4f}")
+
+    # Calcola e visualizza la matrice di confusione
+    cmatrix = confusion_matrix(y_true, y_pred)
+    print(f"\nMatrice di Confusione sul {dataset_type}:")
+    print(cmatrix)
 
 def train_knn(dataset_path):
     print("Preprocessing data..")
@@ -42,34 +61,19 @@ def train_knn(dataset_path):
     knn_classifier = KNeighborsClassifier(n_neighbors=7)
     knn_classifier.fit(X_train, y_train)
 
+    # Predizioni sul training set
+    print("Predicting on training set..")
+    y_train_pred = knn_classifier.predict(X_train)
+
+    # Valuta sul set di training
+    evaluate_model(y_train, y_train_pred, dataset_type='Training')
+
     # Valuta sul set di test
     print("Predict..")
     y_pred = knn_classifier.predict(X_test)
 
-    # Calcola le metriche di valutazione sul test
-    test_accuracy = accuracy_score(y_test, y_pred)
-    test_precision = precision_score(y_test, y_pred, average='weighted', zero_division=0)
-    test_recall = recall_score(y_test, y_pred, average='weighted', zero_division=0)
-    test_f1 = f1_score(y_test, y_pred, average='weighted', zero_division=0)
-
-    # Stampa i risultati sul test
-    print("\nRisultati sul Test con KNN:")
-    print(f"Accuracy del Test: {test_accuracy:.4f}")
-    print(f"Precisione del Test: {test_precision:.4f}")
-    print(f"Recall del Test: {test_recall:.4f}")
-    print(f"F1 Score del Test: {test_f1:.4f}")
-
-    # Calcola e visualizza la matrice di confusione sul test
-    cmatrix = confusion_matrix(y_test, y_pred)
-    print("\nMatrice di Confusione sul Test:")
-    print(cmatrix)
+    # Valuta sul set di test
+    evaluate_model(y_test, y_pred, dataset_type='Test')
 
 dataset_path = '/home/alessandro/Scrivania/UNISA - Magistrale/Tesi/dataset/merged_output_binary.csv'
-data = pd.read_csv(dataset_path)
-
-count_type_0 = data[data['Type of connection'] == 0].shape[0]
-count_type_1 = data[data['Type of connection'] == 1].shape[0]
-print(f"Numero di righe con 'Type of connection' 0: {count_type_0}")
-print(f"Numero di righe con 'Type of connection' 1: {count_type_1}")
-
 train_knn(dataset_path)
--- code/classifiers/SVM.py
+++ code/classifiers/SVM.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from sklearn.svm import SVC
-from sklearn.preprocessing import MinMaxScaler, OneHotEncoder, LabelEncoder
+from sklearn.preprocessing import MinMaxScaler, LabelEncoder
 from sklearn.compose import ColumnTransformer
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix
@@ -22,14 +22,33 @@ def load_and_preprocess_data(csv_file, test_size=0.2):
     numeric_features = X.columns
     preprocessor = ColumnTransformer(transformers=[('num', MinMaxScaler(), numeric_features)])
 
-    # Suddivisione in training set e test set con stratificazione
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify=y, random_state=42)
+    # Suddivisione in training set e test set con stratificazione e shuffle
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, stratify=y, random_state=42, shuffle=True)
 
     # Applica il preprocessing
     X_train = preprocessor.fit_transform(X_train)
     X_test = preprocessor.transform(X_test)
 
     return X_train, y_train, X_test, y_test
+
+def evaluate_model(y_true, y_pred, dataset_type='Test'):
+    # Calcola le metriche di valutazione
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred, pos_label=1, zero_division=0)
+    recall = recall_score(y_true, y_pred, pos_label=1, zero_division=0)
+    f1 = f1_score(y_true, y_pred, pos_label=1, zero_division=0)
+
+    # Stampa i risultati
+    print(f"\nRisultati sul {dataset_type}:")
+    print(f"Accuracy del {dataset_type}: {accuracy:.4f}")
+    print(f"Precisione del {dataset_type}: {precision:.4f}")
+    print(f"Recall del {dataset_type}: {recall:.4f}")
+    print(f"F1 Score del {dataset_type}: {f1:.4f}")
+
+    # Calcola e visualizza la matrice di confusione
+    cmatrix = confusion_matrix(y_true, y_pred, labels=[0, 1])
+    print(f"\nMatrice di Confusione sul {dataset_type}:")
+    print(cmatrix)
 
 def train_svm(dataset_path):
     print("Preprocessing data..")
@@ -42,27 +61,19 @@ def train_svm(dataset_path):
     svm_classifier = SVC(kernel='rbf')
     svm_classifier.fit(X_train, y_train)
 
+    # Predizioni sul training set
+    print("Predicting on training set..")
+    y_train_pred = svm_classifier.predict(X_train)
+
+    # Valuta sul set di training
+    evaluate_model(y_train, y_train_pred, dataset_type='Training')
+
     # Valuta sul set di test
     print("Predict..")
     y_pred = svm_classifier.predict(X_test)
 
-    # Calcola le metriche di valutazione sul test
-    test_accuracy = accuracy_score(y_test, y_pred)
-    test_precision = precision_score(y_test, y_pred, pos_label=1, zero_division=0)
-    test_recall = recall_score(y_test, y_pred, pos_label=1, zero_division=0)
-    test_f1 = f1_score(y_test, y_pred, pos_label=1, zero_division=0)
-
-    # Stampa i risultati sul test
-    print("\nRisultati sul Test con SVM:")
-    print(f"Accuracy del Test: {test_accuracy:.4f}")
-    print(f"Precisione del Test: {test_precision:.4f}")
-    print(f"Recall del Test: {test_recall:.4f}")
-    print(f"F1 Score del Test: {test_f1:.4f}")
-
-    # Calcola e visualizza la matrice di confusione sul test
-    cmatrix = confusion_matrix(y_test, y_pred, labels=[0, 1])
-    print("\nMatrice di Confusione sul Test:")
-    print(cmatrix)
+    # Valuta sul set di test
+    evaluate_model(y_test, y_pred, dataset_type='Test')
 
 dataset_path = '/home/alessandro/Scrivania/UNISA - Magistrale/Tesi/dataset/merged_output_binary.csv'
 train_svm(dataset_path)
